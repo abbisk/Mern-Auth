4cb6176b92cc6a0b1b9b47c8287252e8587f1e60
abbisk <abbiskpro@gmail.com>
2023-07-14 14:21:00 +0530

    latest changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
+import asyncHandler from "express-async-handler";
 // Auth user/set Token
 // POST /api/user/
 
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 export { authUser };
